1e305916b0ec3645a9fa1a22c8437d4aef15ab61
hwan3526 <hwan3526@gmail.com>
2023-07-25 16:00:36 +0900

    Add: 누락된 코드라인 추가

--- js/js_1.js
+++ js/js_1.js
@@ -8,23 +8,25 @@ class VideoInfo {
     loadVideoInfo() {
         let xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function () {
-            if (xhr.status === 200) {
-                let data = JSON.parse(xhr.responseText);
-                if (data.Response === 'False') {
-                    console.log('Fail to fetch video info. Response is \'False\'');
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    let data = JSON.parse(xhr.responseText);
+                    if (data.Response === 'False') {
+                        console.log('Fail to fetch video info. Response is \'False\'');
+                    } else {
+                        console.log('Success to fetch video info.');
+                        this.imageLink = data.image_link;
+                        this.uploadDate = data.upload_date;
+                        this.videoChannel = data.video_channel;
+                        this.videoDetail = data.video_detail;
+                        this.videoLink = data.video_link;
+                        this.videoTag = data.video_tag;
+                        this.videoTitle = data.video_title;
+                        this.views = data.views;
+                    }
                 } else {
-                    console.log('Success to fetch video info.');
-                    this.imageLink = data.image_link;
-                    this.uploadDate = data.upload_date;
-                    this.videoChannel = data.video_channel;
-                    this.videoDetail = data.video_detail;
-                    this.videoLink = data.video_link;
-                    this.videoTag = data.video_tag;
-                    this.videoTitle = data.video_title;
-                    this.views = data.views;
+                    console.log('Fail to fetch video info.');
                 }
-            } else {
-                console.log('Fail to fetch video info.');
             }
         };
         xhr.open('GET', 'https://oreumi.appspot.com/video/getVideoInfo?video_id=' + encodeURIComponent(this.videoId), true);
