ed17552a5f5190d53953786c711aa146b2e2e74b
animalwook <youngwook616@gmail.com>
2023-07-27 17:19:22 +0900

    Revert "Add navbar on/off"
    This reverts commit 9c9746c5bfe5690a7a5106b9ab956391e3c015fd.

--- js/js_2.js
+++ js/js_2.js
@@ -1,69 +1,61 @@
-// // header
+// header
 
-// // hamburger 버튼으로 navbar 요소의 display 값 변환 (none <----> block)
+// hamburger 버튼으로 navbar 요소의 display 값 변환 (none <----> block)
 
-// function hamburgerMobileFunc() {
-//   const navbarFunc = document.querySelector(".navbar");
+function hamburgerMobileFunc() {
+  const navbarFunc = document.querySelector(".navbar");
 
-//   // 현재 요소의 display 상태를 가져옵니다.
-//   const currentDisplay = window.getComputedStyle(navbarFunc).display;
+  // 현재 요소의 display 상태를 가져옵니다.
+  const currentDisplay = window.getComputedStyle(navbarFunc).display;
 
-//   // display 속성을 토글합니다.
-//   if (currentDisplay === "none") {
-//     navbarFunc.style.display = "block";
-//   } else {
-//     navbarFunc.style.display = "none";
-//   }
-// }
-
-// function hamburgerFunc() {
-//   const navbarFunc = document.querySelector(".navbar");
-//   const navbarSmallFunc = document.querySelector(".navbar-small");
-
-//   // 현재 요소의 display 상태를 가져옵니다.
-//   const currentDisplay = window.getComputedStyle(navbarFunc).display;
-
-//   // display 속성을 토글합니다.
-//   if (currentDisplay === "none") {
-//     navbarFunc.style.display = "block";
-//     navbarSmallFunc.style.display = "none";
-//   } else {
-//     navbarFunc.style.display = "none";
-//     navbarSmallFunc.style.display = "block";
-//   }
-// }
-
-// document.querySelector(".hamburger").onclick = hamburgerFunc;
-
-// // notification 창 활성화 (미완성입니다)
-// //active_notifications.svg 생성 필요
-
-// function notificationFunc() {
-//   const notificationIcon = document.querySelector(".notifications");
-
-//   // active 클래스가 이미 존재하면 제거, 존재하지 않으면 추가
-//   notificationIcon.classList.toggle("active");
-
-//   // css
-//   /*
-//     기본 아이콘 이미지
-//     .notifications {
-//     background-image: url('./img/notifications.svg');
-//   }
-  
-//    클릭시 바뀔 아이콘 이미지
-//   .notifications.active {
-//     background-image: url('./img/active_notifications.svg');
-//   }
-//      */
-// }
-
-
-/* ---------------navbar on/off------------- */
-function toggleNavbar() {
-  var navbar = document.querySelector(".navbar");
-  var expandNavbar = document.querySelector(".expand-navbar");
-
-  navbar.classList.toggle("displayNone");
-  expandNavbar.classList.toggle("display");
+  // display 속성을 토글합니다.
+  if (currentDisplay === "none") {
+    navbarFunc.style.display = "block";
+  } else {
+    navbarFunc.style.display = "none";
+  }
 }
+
+function hamburgerFunc() {
+  const navbarFunc = document.querySelector(".navbar");
+  const navbarSmallFunc = document.querySelector(".navbar-small");
+
+  // 현재 요소의 display 상태를 가져옵니다.
+  const currentDisplay = window.getComputedStyle(navbarFunc).display;
+
+  // display 속성을 토글합니다.
+  if (currentDisplay === "none") {
+    navbarFunc.style.display = "block";
+    navbarSmallFunc.style.display = "none";
+  } else {
+    navbarFunc.style.display = "none";
+    navbarSmallFunc.style.display = "block";
+  }
+}
+
+document.querySelector(".hamburger").onclick = hamburgerFunc;
+
+// notification 창 활성화 (미완성입니다)
+//active_notifications.svg 생성 필요
+
+function notificationFunc() {
+  const notificationIcon = document.querySelector(".notifications");
+
+  // active 클래스가 이미 존재하면 제거, 존재하지 않으면 추가
+  notificationIcon.classList.toggle("active");
+
+  // css
+  /*
+    기본 아이콘 이미지
+    .notifications {
+    background-image: url('./img/notifications.svg');
+  }
+  
+   클릭시 바뀔 아이콘 이미지
+  .notifications.active {
+    background-image: url('./img/active_notifications.svg');
+  }
+     */
+}
+
+ 
