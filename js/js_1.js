00adf893d1227686c4cf5c86a71ddfaa0f0fa27b
animalwook <youngwook616@gmail.com>
2023-07-28 17:19:24 +0900

    Add: 채널 포스트로 가져오기 미완성

--- js/js_1.js
+++ js/js_1.js
@@ -182,24 +182,39 @@ document.addEventListener('DOMContentLoaded', async () => {
 ////////////////////////////////////////////////////
 //user 객체를 본문에 실어 보내
 
+
 async function getChannerInfo(Channer) {
     const apiCUrl = `http://oreumi.appspot.com/channel/getChannelInfo?video_channel=${Channer}`;
     const response = await fetch(apiCUrl);
     const ChannerInfoData = await response.json();
+    console.log(ChannerInfoData);
     return ChannerInfoData;
 }
 
 async function postChannerInfo(Channer) {
-    console.log(Channer);
-    const postResponse = await fetch(
-        `http://oreumi.appspot.com/channel/getChannelInfo?video_channel=${Channer}`, {
-            method: "POST",    
-            headers: {
-            "Content-Type": "application/json",
-            },
-            body: JSON.stringify(getChannerInfo(Channer)),
-        }).then((response) => console.log(response));
-    console.log(postResponse)
+    let apiUrl = 'http://oreumi.appspot.com/channel/getChannelInfo';  // 요청을 보낼 URL입니다.
+
+    let jsonData = {"video_channel": Channer}  // 요청에 포함할 데이터를 정의합니다.
+    
+    fetch(apiUrl, {
+      method: 'POST',  // 요청 방식을 POST로 설정합니다.
+      headers: {
+        'Content-Type': 'application/json',  // 요청의 헤더를 설정합니다.
+      },
+      body: JSON.stringify(jsonData),  // 요청 본문에 데이터를 JSON 형식으로 포함합니다.
+    })
+    .then(response => response.json())  // 응답을 JSON 형식으로 파싱합니다.
+    .then(response => {
+      // 데이터가 존재하는지 확인합니다.
+      if (response && response.channel_name !== undefined) {
+        // 각 데이터를 콘솔에 출력합니다.
+        console.log(response.channel_name);
+        console.log(response.banner);
+        console.log(response.profile);
+        console.log(response.subscribers);
+      }
+    })
+    .catch(error => console.error('Error:', error));  // 에러를 콘솔에 출력합니다.
 }
 
 // fetchAuthorName(1).then((name) => console.log("name:", name));
@@ -214,8 +229,9 @@ async function postChannerList() {
 //home.html에서 채널 프로필을 누르면 채널 페이지로 이동
 function goToChannel(info) {
     link = 'channel.html';
-	location.href = link;
-    // postChannerInfo(info)
+    postChannerInfo(info);
+	 location.href = link;
+    // getChannerInfo(info);
 }
 
 function goToVideo(info) {
