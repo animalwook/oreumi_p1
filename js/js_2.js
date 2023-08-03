deefec8c569728a005f56f937631f1f6853be33d
animalwook <youngwook616@gmail.com>
2023-08-03 15:12:41 +0900

    fix:반응형 거진완료

--- js/js_2.js
+++ js/js_2.js
@@ -1,164 +1,70 @@
-// // header
-
-// // hamburger 버튼으로 navbar 요소의 display 값 변환 (none <----> block)
-
-// function hamburgerMobileFunc() {
-//   const navbarFunc = document.querySelector(".navbar");
-
-//   // 현재 요소의 display 상태를 가져옵니다.
-//   const currentDisplay = window.getComputedStyle(navbarFunc).display;
-
-//   // display 속성을 토글합니다.
-//   if (currentDisplay === "none") {
-//     navbarFunc.style.display = "block";
-//   } else {
-//     navbarFunc.style.display = "none";
-//   }
-// }
-
-// function hamburgerFunc() {
-//   const navbarFunc = document.querySelector(".navbar");
-//   const navbarSmallFunc = document.querySelector(".navbar-small");
-
-//   // 현재 요소의 display 상태를 가져옵니다.
-//   const currentDisplay = window.getComputedStyle(navbarFunc).display;
-
-//   // display 속성을 토글합니다.
-//   if (currentDisplay === "none") {
-//     navbarFunc.style.display = "block";
-//     navbarSmallFunc.style.display = "none";
-//   } else {
-//     navbarFunc.style.display = "none";
-//     navbarSmallFunc.style.display = "block";
-//   }
-// }
-
-// document.querySelector(".hamburger").onclick = hamburgerFunc;
-
-// // notification 창 활성화 (미완성입니다)
-// //active_notifications.svg 생성 필요
-
-// function notificationFunc() {
-//   const notificationIcon = document.querySelector(".notifications");
-
-//   // active 클래스가 이미 존재하면 제거, 존재하지 않으면 추가
-//   notificationIcon.classList.toggle("active");
-
-//   // css
-//   /*
-//     기본 아이콘 이미지
-//     .notifications {
-//     background-image: url('./img/notifications.svg');
-//   }
-  
-//    클릭시 바뀔 아이콘 이미지
-//   .notifications.active {
-//     background-image: url('./img/active_notifications.svg');
-//   }
-//      */
-// }
-
-
-/* ---------------navbar on/off------------- */
+/////////////////////////////////////////////
+/* navBarB가 켜져있는 경우 복구*/ 
+/*pading 값의 경우 값이 고정 되어 버리니까 css에서 인식 주의*/ 
 window.addEventListener('resize', function() {
-  if (window.innerWidth < 900) {
-      const navBarA = document.getElementById('navBarA');
-      if (navBarA) navBarA.style.display = 'none';
-
-      const navBarB = document.getElementById('navBarB');
-      if (navBarB) navBarB.style.display = 'none';
-  }
-  else {
-      const navBarA = document.getElementById('navBarA');
-      if (navBarA) navBarA.style.display = '';
-
-      const navBarB = document.getElementById('navBarB');
-      if (navBarB) navBarB.style.display = '';
-  }
-});
-
-document.getElementById('hamburger').addEventListener('click', function() {
   const navBarA = document.getElementById('navBarA');
   const navBarB = document.getElementById('navBarB');
+  const navBarC = document.getElementById('navBarC');
   const section = document.getElementById('section');
 
-  if(window.innerWidth >= 900){
-    if (navBarA.classList.contains('active')) {
-      navBarA.classList.remove('active');
-      navBarB.classList.add('active');
-      section.style.paddingLeft = "240px";
-    } else {
-      navBarB.classList.remove('active');
-      navBarA.classList.add('active');
-      section.style.paddingLeft = "80px";
-    }
+ if (window.innerWidth <= 1366) {
+  navBarB.classList.remove('active');
+  navBarA.classList.add('active');
+  section.style.paddingLeft = "80px";
+  navBarC.classList.remove('active');
+ }
+ if (window.innerWidth <= 600) {
+  section.style.paddingLeft = "5px";
+ }
+});
+
+/////////////////////////////////////////////
+/* 햄버거 온오프 기능 적용됨*/ 
+document.getElementById('hamburger').addEventListener('click', function() {
+const navBarA = document.getElementById('navBarA');
+const navBarB = document.getElementById('navBarB');
+const navBarC = document.getElementById('navBarC');
+const section = document.getElementById('section');
+
+
+/*1366이상에서는 온오프기능으로*/ 
+if(window.innerWidth > 1366 ){
+  if (navBarC.classList.contains('active')) {
+    navBarC.classList.remove('active');
   }
-});
 
-document.getElementById('navBarHamburger').addEventListener('click', function() {
-  const navBarC = document.getElementById('navBarC');
-  const overlay = document.getElementById('overlay');
-
-  navBarC.classList.remove('active');
-  overlay.classList.remove('visible');
-});
-
-document.getElementById('overlay').addEventListener('click', function() {
-  const navBarC = document.getElementById('navBarC');
-  const overlay = document.getElementById('overlay');
-
-  navBarC.classList.remove('active');
-  overlay.classList.remove('visible');
-});
-
-
-
-function isMiddlePage() {
-  return window.location.pathname.includes("/middle-page");
-}
-
-function showExpandNavbar() {
-  const expandNavbar = document.querySelector(".expand-navbar");
-  expandNavbar.style.display = "block";
-}
-
-function hideExpandNavbar() {
-  const expandNavbar = document.querySelector(".expand-navbar");
-  expandNavbar.style.display = "none";
-}
-
-// 페이지 사이즈별 구분
-function controlScreen() {
-  const navbar = document.querySelector(".navbar");
-  const hamburger = document.querySelector(".hamburger");
-  const screenWidth = window.innerWidth;
-
-  // 모든 페이지에서 햄버거 버튼은 보여짐 // 최소화면에서 햄버거가 계속 사라져서 강제시켰습니다.
-  hamburger.style.display = "block";
-
-  if (isMiddlePage() && screenWidth > 768) {
-    showExpandNavbar();
-    navbar.style.display = "none";
-  } else {
-    hideExpandNavbar();
-    navbar.style.display = "block";
+  else if (navBarA.classList.contains('active')) {
+    navBarA.classList.remove('active');
+    navBarB.classList.add('active');
+    section.style.paddingLeft = "240px";
+    navBarC.classList.remove('active');
+  } 
+  else  {
+    navBarB.classList.remove('active');
+    navBarA.classList.add('active');
+    section.style.paddingLeft = "80px";
   }
 }
+/*1366 아닌경우 창 나타나기로*/
+if(window.innerWidth <= 1366){
+    if (navBarC.classList.contains('active')) {
+      navBarC.classList.remove('active');
+  } else{
+    navBarC.classList.add('active');
+  }
+}
+});
 
-window.addEventListener("load", controlScreen);
-window.addEventListener("resize", controlScreen);
-
-controlScreen();
 
 // Top-Menu 스크롤 버튼 구현
 const slideLeft = document.getElementById("slide-left");
 
 slideLeft.onclick = () => {
-  document.getElementsByClassName("button")[0].scrollLeft -= 300;
+document.getElementsByClassName("tag_button")[0].scrollLeft -= 300;
 };
 
 const slideRight = document.getElementById("slide-right");
 
 slideRight.onclick = () => {
-  document.getElementsByClassName("button")[0].scrollLeft += 300;
+document.getElementsByClassName("tag_button")[0].scrollLeft += 300;
 };
